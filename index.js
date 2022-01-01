d765fd858708ae02a953d3aca6ce4fa3487f814b
Chayti Saha <chaytisaha98@gmail.com>
2022-01-01 17:28:33 +0600

    pament system added

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express()
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
-// const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +20,7 @@ async function run() {
         const database = client.db("heroRider");
         const usersCollection = database.collection("users");
         const LessonsCollection = database.collection("lessons");
+        // const bookingCollection = database.collection("booking");
 
         // add user to DB
         app.post('/users', async (req, res) => {
@@ -54,6 +56,49 @@ async function run() {
             const cursor = LessonsCollection.find({})
             const lessons = await cursor.toArray()
             res.send(lessons)
+        })
+
+        // app.post('/booking', async (req, res) => {
+        //     const booking = req.body
+        //     const result = await bookingCollection.insertOne(booking)
+        //     res.json(result)
+        // })
+
+        // app.get('/booking/:email', async (req, res) => {
+        //     const cursor = bookingCollection.find({})
+        //     const booking = await cursor.toArray()
+        //     res.send(booking)
+        // })
+
+        app.get('/lessons/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await LessonsCollection.findOne(query);
+            res.json(result);
+        })
+
+        app.put('/lessons/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    payment: payment
+                }
+            };
+            const result = await LessonsCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const paymentInfo = req.body;
+            const amount = paymentInfo.price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                payment_method_types: ['card']
+            });
+            res.json({ clientSecret: paymentIntent.client_secret })
         })
 
     }
